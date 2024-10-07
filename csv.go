e2b521e9d71e8912902ff90a8acdac87047d8784
tgt <kasyap3103@gmail.com>
2024-10-07 14:35:31 +0530

    feature working on

--- csv.go
+++ csv.go
@@ -1,9 +1,46 @@
-package main 
-
+package main
 
 import (
 	"encoding/csv"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
 	"os"
-    
-	
 )
+
+type Anime struct {
+	ID          uint   `gorm:"primaryKey"`
+	Title       string
+	Description string
+	Genre       string
+	ReleaseDate string
+	Rating      float64
+	Episodes    int
+	Studio      string
+	Status      string
+	ImageURL    string
+}
+
+func read_csv() {
+	file, err := os.Open("anime-2.csv")
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db, err := gorm.Open(sqlite.Open("anime.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.AutoMigrate(&Anime{})
+     for _,record :=range records[1:]{
+		anime :=Anime{
+			
+		}
+	 }
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/kasyap1234/anime-api
 
 go 1.23.2
 
-require github.com/go-chi/chi/v5 v5.1.0
+require (
+	github.com/go-chi/chi/v5 v5.1.0
+	gorm.io/driver/sqlite v1.5.6
+	gorm.io/gorm v1.25.12
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
